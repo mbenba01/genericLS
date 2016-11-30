2812b6a8a04c16c0abd9a57e936de454905f1e27
mbenba01 <m.benbaziz@hotmail.co.uk>
2016-11-30 14:29:42 +0000

    First commit to the repo to add index.php and Readme.txt

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Project: GENERIC LOGIN SYSTEM
+ * Author: MUSTAPHA BENBAZIZ
+ * Date: 30/11/2016
+ * Description: The aim of this project is to develop using php, HTML 5 and CSS 3
+ * 
+ * File name: index.php
+ * This is the index page which will be used as a single point of entry.
+ * It must include a login form, a header section, a primary menu and a content area.
+ * 
+ */
+
+// include header
+
+
+
+// echo content
+
+
+
+// include footer
