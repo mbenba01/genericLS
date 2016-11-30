f0a467e6deec1855595b529ed6d9992c931e4d5c
mbenba01 <m.benbaziz@hotmail.co.uk>
2016-11-30 17:14:04 +0000

    Added a new file in the modules folder: main_menu.php

---
+++ modules/main_menu.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$main_menu = 'html/main_menu_tpl.html';
+	$tpl = file_get_contents($main_menu);
+	
+	$homepage = 'index.php?SID';
+	$public = 'public.php?SID';
+	$private = 'private.php?SID';
+	$members = 'members.php?SID';
+	
+	$pass1 = str_replace('[+homepage+]', $homepage, $tpl);
+	$pass2 = str_replace('[+public+]', $public, $pass1);
+	$pass3 = str_replace('[+private+]', $private, $pass2);
+	$final = str_replace('[+members+]', $members, $pass3);
+	
+	$content .= $final;
