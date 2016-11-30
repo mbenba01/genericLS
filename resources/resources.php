b1217f18fcc465a590372149074a9dcc4551001b
mbenba01 <m.benbaziz@hotmail.co.uk>
2016-11-30 17:28:44 +0000

    Changed the second css link to link to the renamed "global.css" file

--- resources/resources.php
+++ resources/resources.php
@@ -1,2 +1,2 @@
 <link href="http://localhost/phpapplications/genericLS/css/reset.css" rel="stylesheet">
-<link href="http://localhost/phpapplications/genericLS/css/style.css" rel="stylesheet">
+<link href="http://localhost/phpapplications/genericLS/css/global.css" rel="stylesheet">
