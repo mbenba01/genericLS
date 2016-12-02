7703bf6abe9a17474ec06072694a8579b5add19d
mbenba01 <m.benbaziz@hotmail.co.uk>
2016-12-02 10:32:04 +0000

    Added more abstraction to the login form code

--- modules/login_form.php
+++ modules/login_form.php
@@ -1,12 +1,16 @@
 <?php
-	
+		
 	$login_form = 'html/login_form_tpl.html';
 	$tpl = file_get_contents($login_form);
 	
 	$self = htmlentities ( $_SERVER ['PHP_SELF'] );
 	$name = 'login-form';
+	$label_one = 'Username';
+	$label_two = 'Password';
 
 	$pass1 = str_replace('[+action+]', $self, $tpl);
-	$final = str_replace('[+name+]', $name, $pass1);
+	$pass2 = str_replace('[+name+]', $name, $pass1);
+	$pass3 = str_replace('[+labelOne+]', $label_one, $pass2);
+	$final = str_replace('[+labelOne+]', $label_two, $pass3);
 
 	echo $final;
