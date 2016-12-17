80676cd7626b0a9fd75cdbbd234bcd0cef4fb28f
mbenba01 <m.benbaziz@hotmail.co.uk>
2016-12-17 12:03:58 +0000

    Commit changes

--- modules/login_form.php
+++ modules/login_form.php
@@ -7,10 +7,12 @@
 	$name = 'login-form';
 	$label_one = 'Username';
 	$label_two = 'Password';
+	$registration_page = 'register.php?' . SID;
 
 	$pass1 = str_replace('[+action+]', $self, $tpl);
 	$pass2 = str_replace('[+name+]', $name, $pass1);
 	$pass3 = str_replace('[+labelOne+]', $label_one, $pass2);
-	$final = str_replace('[+labelTwo+]', $label_two, $pass3);
+	$pass4 = str_replace('[+labelTwo+]', $label_two, $pass3);
+	$final = str_replace('[+register+]', $registration_page, $pass4);
 
 	echo $final;
