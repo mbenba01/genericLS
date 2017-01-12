61832ebe2bbc97df9e4fc27f6c76b0c48bae322c
mbenba01 <m.benbaziz@hotmail.co.uk>
2017-01-12 21:06:51 +0000

    Corrected a typo with the name of the form

--- modules/registration_form.php
+++ modules/registration_form.php
@@ -1,1 +1,12 @@
 <?php
+	
+	$registration_form = 'html/registration_form_tpl.html';
+	$tpl = file_get_contents ( $registration_form );
+	
+	$self = htmlentities ( $_SERVER ['PHP_SELF'] );
+	$name = 'registration-form';
+	
+	$pass1 = str_replace ( '[+action+]', $self, $tpl );
+	$final = str_replace ( '[+name+]', $name, $pass1 );
+	
+	echo $final;
